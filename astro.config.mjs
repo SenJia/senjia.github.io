681f384522478064d58ff9d51295c92b76766e93
sen.jia <jason.senjia@gmail.com>
2025-03-04 22:22:27 -0500

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/SenJia/senjia.github.io',
+  site: 'github.com/SenJia/senjia.github.io',
+  integrations: [mdx(), sitemap(), tailwind()]
 });
